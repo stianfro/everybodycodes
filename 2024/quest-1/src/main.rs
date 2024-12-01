14b4a6343be3a5673b5f38f4f7ddd6fbf6b13d23
Stian Frøystein <stianfroy@gmail.com>
2024-12-01 13:14:30 +0900

    feat(2024/day-1): add wip part 2

--- 2024/quest-1/src/main.rs
+++ 2024/quest-1/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     let p1 = part_one("AACBCCAACCCACABBAABBAABBBBCCCCABBACABCABCCBBCAAACBBACABAAACCCBBBACBCCACBCCACAACBAACCBCACBCABAABBACBCAACBBCBBBABCCBCCBCAAACCCCABBABABAABBABACCAABBCCABAABCBABCCACCBACCBABBCAAACBABBAACCCCCBACABCAABCCBBBCAAAACCBCABCCBBBAAABABBCAACCABACCABBACCBAABCCABCACBBBACBACACCAACACCACCBBCCBACACBCCBCCBBCCACAAABABBCCBBCCABCACCABACABBBBABBACBCBBAAABBBCCCAABCCCAAAACCBCAABAABBCCACBCCCBCBBBBABBACCBCCCACCAAABBAACACABBAACAACACBABABCAABCBAABAABBACACABABABCACCBCCAACBAACCCCBBBBCBAAABAACBBCBBAAACBCCAAAAABBCACCCCABBBABCBABBBABBCCBACABCACCCACBBBABACBABABACCBAACBCABBACCCBBCABCCAABAAAACCBABABABCCBABBABCCCBCCABAACACCACBCAACAACCCAABABABBACCCCBABBCCBCBCBACBABCBBBBACCACBACAABBBCACCACBCABBCBCCACCBACBBACBBCCCAACABCBCCCCCBBCCABCACBACBCCABCAABCAACBBABAACBBACCAAAACACCABCBBCABCCAABAABBCABCCAAACABCBBACCCCBBBACBCCBAACBABAACBBBCCBBCCBBABBACBBAABCBBCBAAAABACBCBABCBCBACBBBBBAAACACBCCCCBACBACCAACACABCCBBACBBABCACACCBBAABAACBBABCCCABBABCAAAAABCBCCBCAABAABCCACCCCCABBBBBCCAACBCBBCAAACBAAAAAACAABCAABBABBABCAAABBABACCAAACCAAACCACCABCCAABC");
-    // let p2 = part_two("AxBCDDCAxD");
+    let p2 = part_two("AxBCDDCAxD");
 
-    println!("Part 1: {p1}")
+    println!("Part 1: {p1}");
+    println!("Part 2: {p2}");
 }
 
 fn part_one(input: &str) -> i32 {
@@ -33,13 +34,41 @@ fn part_one(input: &str) -> i32 {
     potions
 }
 
-// fn part_two(input: &str) {
-//     // iterate list, add char to slice, when slice contains 2 chars add to listPairs
-//
-//     let list = input.split("");
-//
-//     let pairs: Vec<&str> = Vec::new();
-// }
+fn part_two(input: &str) -> i32 {
+    let list = input.split("");
+
+    let mut creatures: Vec<&str> = Vec::new();
+
+    for creature in list {
+        if creature == "\n" || creature == "" {
+            continue;
+        }
+
+        creatures.push(creature);
+    }
+
+    let mut pairs: Vec<Vec<&str>> = Vec::new();
+
+    for i in 0..creatures.len() {
+        if i % 2 == 0 {
+            let c0 = creatures[i];
+            let c1 = creatures[i + 1];
+
+            let p = vec![c0, c1];
+
+            pairs.push(p);
+        }
+    }
+
+    for pair in pairs {
+        let p0 = pair[0];
+        let p1 = pair[1];
+
+        println!("{p0}{p1}")
+    }
+
+    1
+}
 
 #[cfg(test)]
 mod tests {
@@ -49,5 +78,11 @@ mod tests {
     fn test_part_one() {
         let result = part_one("ABBAC");
         assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two("AxBCDDCAxD");
+        assert_eq!(result, 28)
     }
 }
